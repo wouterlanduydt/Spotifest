1b5d93a28d39dfecdda575177b23032f5e47a650
Wouter Landuydt <wouter.landuydt@novemberfive.co>
2019-05-16 00:13:51 +0200

    :lipstick: basic styling of artist items

--- src/components/ArtistItem.tsx
+++ src/components/ArtistItem.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 
 type TProps = {
   artist: SpotifyApi.ArtistObjectFull;
+  position: number;
 };
 
-const ArtistItem = ({ artist: { name, external_urls } }: TProps) => (
-  <div>
-    <a href={external_urls.spotify} rel="noopener noreferrer">
-      {name}
-    </a>
-  </div>
+const getFontSize = (pos: number) => {
+  if (pos <= 12) return '2.8vw';
+  if (pos > 12 && pos <= 26) return '2vw';
+  if (pos > 26) return '1.8vw';
+};
+
+const Wrap = styled.li<{ position: number }>`
+  margin: 4px;
+  font-size: ${({ position }) => getFontSize(position)};
+`;
+
+const ArtistItem = ({ artist: { name, external_urls }, position }: TProps) => (
+  <>
+    <Wrap position={position}>
+      <a href={external_urls.spotify} rel="noopener noreferrer">
+        {name}
+      </a>
+    </Wrap>
+    {(position === 12 || position === 26) && <div style={{ width: '100%' }} />}
+  </>
 );
 
 export default ArtistItem;
--- src/components/Poster.tsx
+++ src/components/Poster.tsx
@@ -5,6 +5,7 @@ import { ESortCriteria } from 'types/general';
 import { TTopArtists } from 'redux/reducers';
 import ArtistItem from './ArtistItem';
 import { getSortedArtists } from 'lib';
+import styled from 'styled-components';
 
 type TProps = {
   username: string | undefined | null;
@@ -12,19 +13,40 @@ type TProps = {
   sortCriteria: ESortCriteria;
 };
 
+const Wrap = styled.div`
+  width: 96vw;
+  max-width: 600px;
+  min-height: 400px;
+  margin: 0 auto;
+  border: 2px solid black;
+  padding: 16px;
+`;
+
+const ArtistsWrap = styled.ol`
+  display: flex;
+  width: 90%;
+  margin: 0 auto;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
 const Poster = ({ artists, sortCriteria, username }: TProps) => {
   return (
-    <div>
+    <Wrap>
       <Title title={`${username}'s Spotifest`} />
-      {artists.value &&
-        getSortedArtists(artists.value, sortCriteria).map(artist => <ArtistItem artist={artist} />)}
+      <ArtistsWrap>
+        {artists.value &&
+          getSortedArtists(artists.value, sortCriteria).map((artist, i) => (
+            <ArtistItem key={artist.id} artist={artist} position={i} />
+          ))}
+      </ArtistsWrap>
 
       {/* <div>
         <a href="https://www.spotify.com" rel="noopener noreferrer">
           <img src={SpotifyLogo} alt="" />
         </a>
       </div> */}
-    </div>
+    </Wrap>
   );
 };
 
